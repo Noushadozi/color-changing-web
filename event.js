afb8c3bed25c9f9ec37424a459c4c5e0b4743619
Noushadozi <noushadozi333@gmail.com>
2023-03-03 15:05:08 +0600

    ok

--- event.js
+++ event.js
@@ -17,6 +17,25 @@ document.getElementById("btn-success").addEventListener("click", function(){
         document.body.style.backgroundColor = "black";
         document.getElementById('h2-container').innerText = "ghunghur";
 });
+function makeRed(){
+    document.body.style.backgroundColor = "red";
+    document.getElementById("heading").innerText = "Testing Event Onclick";
+}
+
+function makeGreen(){
+    document.body.style.backgroundColor = "green";
+    document.getElementById("heading").innerText = "success button clicked";
+}
+
+const myEvent2 = document.getElementById("blue-btn");
+myEvent.addEventListener("click", function(){
+    document.body.style.backgroundColor = "tomato";
+    document.getElementById('h2-container').innerText = "Hello Teddy"
+})
+document.getElementById("btn-success").addEventListener("click", function(){
+        document.body.style.backgroundColor = "black";
+        document.getElementById('h2-container').innerText = "ghunghur";
+});
 
 console.log("javascript");
 console.log("javascript");
